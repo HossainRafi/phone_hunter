8ced0ef35c2e447c052a495ecf21cf49cfc12f99
HossainRafi <hossainrafi71@gmail.com>
2022-03-01 12:46:32 +0600

    get phone details

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const getValue = () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((info) => displaySearchResult(info.data));
+    .then((data) => displaySearchResult(data.data));
 };
 
 const displaySearchResult = (phones) => {
-  //   console.log(phones);
-    const searchResult = document.getElementById("search-result");
+  const searchResult = document.getElementById("search-result");
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
@@ -21,10 +20,19 @@ const displaySearchResult = (phones) => {
           <div class="card-body">
             <h3 class="card-title">Model: ${phone.phone_name}</h3>
             <h4 class="card-title">Brand: ${phone.brand}</h4>
-            <button type="button" class="btn btn-secondary">Phone Datails</button>
+            <button onclick="phoneDetail('${phone.slug}')" type="button" class="btn btn-secondary">
+            Phone Datails
+            </button>
           </div>
         </div>
         `;
     searchResult.appendChild(div);
   });
+};
+const phoneDetail = (phoneId) => {
+  // console.log(phoneId);
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
 };
